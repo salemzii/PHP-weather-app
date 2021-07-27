fab877ad6190b0c8c55417babcc119d9bcd60088
Jindullahi1 <86887816+Jindullahi1@users.noreply.github.com>
2021-07-27 17:41:31 +0100

    jindullahi commiting

---
+++ index2.php
@@ -0,0 +1,12 @@
+<table class="table table-striped">
+    <thead class="thead-dark">
+        <tr>
+        <th scope="col">City</th>
+        <th scope="col">Temperature in Farenheit</th>
+        <th scope="col">Temperature in Celcius</th>
+        <th scope="col">Humidity</th>
+        <th scope="col">Description</th>
+        <th scope="col">Icon</th>            
+        </tr>
+    </thead>
+</table>
